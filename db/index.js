f032a0c5efe54b85f63da9f473ebb55af057a8f6
duanli_666 <316149574@qq.com>
2023-03-02 09:16:00 +0800

    change url

--- db/index.js
+++ db/index.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = ()=>{
-    mongoose.connect('mongodb://zhichuang.team:27017/zhichuang',{useNewUrlParser:true,useUnifiedTopology: true },).then(()=>{
-        console.log("数据库链接成功");
-    }).catch(err=>{
-        console.error('数据库链接失败'+err);
+module.exports = () => {
+  mongoose
+    .connect("mongodb://119.3.202.71:27017/zhichuang", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("数据库链接成功");
+    })
+    .catch((err) => {
+      console.error("数据库链接失败" + err);
     });
-}
+};
